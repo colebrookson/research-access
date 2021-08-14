ebaca35e3ac990f0cc83ef7170888ab96bfdca80
Sukanta Saha <sukanta@ualberta.ca>
2021-08-14 09:50:42 +0530

    Update unnamedModule.py

--- src/pythonScripts/unnamedModule.py
+++ src/pythonScripts/unnamedModule.py
@@ -1,1 +1,41 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def authenticateGDrive(jsonFilename):
+    """
+    Authorises API client information from jsonFilename with Google Cloud Platform
+    returns: authorised client as gspread.Client object
+    """
+    scope = ["https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(jsonFilename, scope)
+    client = gspread.authorize(creds)
+
+    return client
+
+def getData(client, file="", by="name", sheetNum=0):
+    """
+    gets data from sheetNum of file using an authenticated Google Cloud Platform client
+    - client = gspread.Client class 
+    returns: list of each row in the spreadsheet as a dict with each col as key
+    - example: [{"Serial No": 0, "Name": "Abby"}, {"Serial No": 1, "Name": "Jason"}]
+    """
+    assert type(file) == str, "file must be a str"
+    assert type(sheetNum) == int and sheetNum >= 0, "sheetNum must be an int and at least 0"
+    assert by in ["name", "id", "url"], "by must be one of name, id or url"
+
+    if by == "name":
+        sheets = client.open(file)
+    elif by == "id":
+        sheets = client.open_by_key(file)
+    elif by == "url":
+        sheets = client.open_by_url(file)
+
+    data = sheets.get_worksheet(sheetNum)
+    data = data.get_all_records()
+
+    return data
+
 
