fa3c77b3689a8f65d9d3bc190a3177bc3bb43d59
Sukanta Saha <sukanta@ualberta.ca>
2021-08-15 10:55:17 +0530

    Update dataChecks.py

--- src/pythonScripts/dataChecks.py
+++ src/pythonScripts/dataChecks.py
@@ -5,7 +5,6 @@ def validate_cISSN(issn:str) -> bool:
     """
     assert type(issn) == str, "issn must be a string"
 
-    isVALID_C = False
     issn_num = issn[:4] + issn[5:-1]
     issn_c = issn[-1]
 
@@ -16,15 +15,11 @@ def validate_cISSN(issn:str) -> bool:
         num = int(num)
         issn_num_sum += num*inv_index
         inv_index -= 1
-
+        
     mod = issn_num_sum%11
-    if mod == 0: 
-        c = 0
+    if mod == 0: c = 0
     else:
         c = 11-mod
         if c == 10: c = 'X'
 
-    if str(c) == issn_c: isVALID_C = True
-
-    return isVALID_C
-
+    return str(c) == issn_c
