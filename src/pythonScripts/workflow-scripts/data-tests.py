da5255b39ef7137bd68088b18e4f4dd6bad07538
Sukanta Saha <sukanta@ualberta.ca>
2021-08-16 06:29:43 +0530

    testing

--- src/pythonScripts/workflow-scripts/data-tests.py
+++ src/pythonScripts/workflow-scripts/data-tests.py
@@ -1,5 +1,8 @@
 # this script should run every time there is a new push to the data folder
+import pandas as pd
 
-PATH = "data//from-GDrive//mainDB.csv"
-fh = open(PATH)
-print(fh)
+PATH_mainDB = "data//from-GDrive//mainDB.csv"
+fh = open(PATH_mainDB)
+
+df = pd.DataFrame({"col1":[1,2,3], "col2":[4,5,6]})
+pd.to_csv(PATH_mainDB)
