33af6936a899690e95c3cab3c73e6c0fcd4dd96e
Sukanta Saha <sukanta@ualberta.ca>
2021-08-28 22:40:24 +0530

    Update mainScript.py

--- src/pythonScripts/modules/mainScript.py
+++ src/pythonScripts/modules/mainScript.py
@@ -116,7 +116,7 @@ def updateSheetOnDrive(handler, sheetID, ALL_CLEANED_SHEETS):
 
 def main():
 
-    print(os.cwd)
+    print(os.getcwd())
     # authenticating Drive, Sheets and GitHub API keys
     sheetsDriveJson = "src/pythonScripts/modules/creds.json"
     driveServiceJson = "src/pythonScripts/modules/client_secrets_GDrive-oauth2.json"
